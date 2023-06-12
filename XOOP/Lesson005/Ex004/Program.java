0172b5fa151ae50848a5cf03911c554812781509
Mels <ilia2577@gmail.com>
2023-06-12 15:45:41 +0800

    Seminar001:start

--- XOOP/Lesson005/Ex004/Program.java
+++ XOOP/Lesson005/Ex004/Program.java
@@ -2,20 +2,20 @@ package Ex004;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) throws IOException {
-        FileWriter writer = new FileWriter("Example2.txt");
-        for (int i = 0; i < 1000; i++) {
-            long time = System.currentTimeMillis();
+        FileWriter writer = new FileWriter("listResult.txt");
+        long time = System.currentTimeMillis();
+        for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
                 System.out.printf("Sum: %d\n", Sum());
             }
-            String endTime = String.format("%d ms\n", System.currentTimeMillis() - time);
-            System.out.print(endTime);
-            writer.write(endTime);
         }
+        String endTime = String.format("%d ms\n", System.currentTimeMillis() - time);
+        System.out.println();
+        System.out.print(endTime);
+        writer.write(endTime);
         writer.close();
     }
 
