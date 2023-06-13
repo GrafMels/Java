d197a3742b68ceb846752f59ab74a2e7adfdda45
Mels <ilia2577@gmail.com>
2023-06-13 16:29:04 +0800

    Lesson002

---
+++ IntelliJ_IDEA/Lesson002/Ex001/src/Main.java
@@ -0,0 +1,16 @@
+/**
+ * Class entry point
+ */
+public class Main {
+    /**
+     * Method entry point
+     * @param args - some args
+     */
+    public static void main(String[] args) {
+        byte b0 = (byte) 200;
+        System.out.println(b0);//Переполнение переменной
+
+
+
+    }
+}
