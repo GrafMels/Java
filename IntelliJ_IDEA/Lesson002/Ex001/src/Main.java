77a45eed9868639647de51bcc84c2d5518086354
Mels <ilia2577@gmail.com>
2023-06-13 19:25:36 +0800

    Seminar001:Homework:End

--- IntelliJ_IDEA/Lesson002/Ex001/src/Main.java
+++ IntelliJ_IDEA/Lesson002/Ex001/src/Main.java
@@ -9,6 +9,11 @@ public class Main {
     public static void main(String[] args) {
         byte b0 = (byte) 200;
         System.out.println(b0);//Переполнение переменной
+        long l0 = 5000000000;
+        long l1 = 5000000000L;
+        float f0 = 0.123;
+        float f1 = 0.123f;
+
 
 
 
